3c170f259a201ff139029e23a59850a5b93c0422
Aina Kobayashi <>
2024-11-29 13:41:30 +0900

    root設定と　バリデーション修正

--- app/models/post.rb
+++ app/models/post.rb
@@ -6,11 +6,9 @@ class Post < ApplicationRecord
     validates :memo, length: {maximum:500}
 
     def start_end_day
-        if !end_day.nil? && !start_day.nil? 
+        if 
             end_day < start_day
             errors.add(:end_day, "は開始日よりも前の日付は使えません")
         end
     end
 end
-
-
